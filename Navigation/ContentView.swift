9e5cc4e5b6f57bd21172a0a4f9d1a9d185a40fd8
kwk Cecilia .C <cc.comar36@gmail.com>
2025-07-24 09:58:31 -0400

    Customizing text on two pages and creating title for home page

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -11,10 +11,19 @@ struct ContentView: View {
     var body: some View {
         NavigationStack{
             VStack {
-                Text("This is the home page 🏠")
+                Text("This is the home page   🏠")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color(hue: 0.539, saturation: 0.756, brightness: 0.551))
                 NavigationLink(destination: SecondView()){
                     Text("Click Here")
+                        .font(.title2)
+                        .fontWeight(.light)
                 }
+                
+                .navigationTitle("Home")
+                .navigationBarTitleDisplayMode(.inline)
+               // .navigationBarHidden(true)
             }
             .padding()
         }
